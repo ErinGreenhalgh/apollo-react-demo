246d21eb7abee148ed0dead67bfed8fa72a3a5c8
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2019-04-07 15:54:36 -0500

    WIP - import apollo and start query setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: 'https://api-useast.graphcms.com/v1/cjtd68sm976tp01dnyxcrrzq3/master',
+})
+
+// what is different about this request than the one gaphCMS is making?
+// how to pass variables into this query?
+client.query({
+  query: gql`
+  {
+    content {
+      addresses {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  }
+  `
+}).then( result => { console.log("RESULT:", result)})
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
