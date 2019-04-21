5a275240aac2e4093f1e83924b884dc890c66810
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2019-04-21 16:15:16 -0500

    render addresses from GQL w/ Query

--- src/addressBook.tsx
+++ src/addressBook.tsx
@@ -1,48 +1,59 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 interface IAddress {
+  id: string;
   name: string;
   streetAddress: string,
   postalCode: string;
   state: string;
-  default: boolean;
+  primary: boolean;
 }
 
-const address: IAddress = {
-  name: "Mr. Guy",
-  streetAddress: '123 Great St.',
-  postalCode: '11111',
-  state: "Colorado",
-  default: true,
-}
-
-export default class AddressBook extends React.Component {
-  constructor() {
-    super({});
-    this.renderAddress = this.renderAddress.bind(this);
-  }
-
-  renderAddress(address: IAddress) {
+const AddressBook = () => {
+  function renderAddress(address: IAddress) {
     return (
-      <React.Fragment>
+      <div className="address-card" key={address.id}>
         <div>{address.name}</div>
         <div>{address.streetAddress}</div>
         <div>{address.postalCode}</div>
         <div>{address.state}</div>
         <div>
           <label htmlFor="default">Default</label>
-          <input id="default" type="checkbox" checked={true}></input>
+          <input id="default" type="checkbox" checked={address.primary}></input>
         </div>
-      </React.Fragment>
+      </div>
     )
   }
-  render() {
-    return (
-      <React.Fragment>
-        <header>My Address Book</header>
-        {this.renderAddress(address)}
+
+  return (
+    <Query 
+    query = {
+      gql`{
+        addresses {
+          streetAddress
+          id
+          primary
+        }
+      }
+      `
+    }
+  >
+    {
+     ({loading, error, data}) => {
+       return(
+        <React.Fragment>
+        {loading && <p>Loading...</p>}
+        {error &&  <p>Error :(</p>}
+
+        {!loading && !error && data.addresses.map((address: IAddress) => renderAddress(address))}
       </React.Fragment>
-    
-    )
-  }
+      );
+     }
+    }
+  </ Query >
+  )
 }
+
+export default AddressBook;
